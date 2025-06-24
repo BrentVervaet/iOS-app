75eb1ca8902c32e12fdc46fcef0853197cf52eef
Brent Vervaet <brent.vervaet@student.hogent.be>
2025-06-24 21:24:16 +0200

    init

---
+++ iOS-app/iOS-app/iOS_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_appApp.swift
+//  iOS-app
+//
+//  Created by Brent Vervaet on 24/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOS_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
